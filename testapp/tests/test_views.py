edf3dde8bd6dd1f154c92b142ffc1af45df0d614
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-23 20:34:45 -0700

    fixed docstrings.

--- testapp/tests/test_views.py
+++ testapp/tests/test_views.py
@@ -67,13 +67,13 @@ def test_home_view_sort_item2_text(new_model):
 
 
 def test_home_route(dbtransaction, app):
-    """Test home view dictionary title attribute."""
+    """Test home route status code."""
     response = app.get('/')
     assert response.status_code == 200
 
 
 def test_new_route(dbtransaction, app):
-    """Test home view dictionary title attribute."""
+    """Test new route status code."""
     response = app.get('/new')
     assert response.status_code == 200
 
@@ -120,7 +120,7 @@ def test_new_post_minimum_text_length(dbtransaction, app):
 
 
 def test_new_post_over_max_title_length(dbtransaction, app):
-    """Test that a new post is created."""
+    """Test that a new post adheres to max title length."""
     mock_title = []
     for i in range(130):
         mock_title.append('a')
@@ -137,7 +137,7 @@ def test_new_post_over_max_title_length(dbtransaction, app):
 
 
 def test_edit_entry(dbtransaction, app, new_model):
-    """Test that a new post is created."""
+    """Test that a post can be edited."""
     params = {
         'title': 'T' + new_model.title,
         'text': 'T' + new_model.text
@@ -149,7 +149,7 @@ def test_edit_entry(dbtransaction, app, new_model):
 
 
 def test_edit_entry_min_title_len(dbtransaction, app, new_model):
-    """Test that a new post is created."""
+    """Test that edited title maintains min length conditions."""
     params = {
         'title': 'T',
         'text': 'Testing'
@@ -159,7 +159,7 @@ def test_edit_entry_min_title_len(dbtransaction, app, new_model):
 
 
 def test_edit_entry_min_text_len(dbtransaction, app, new_model):
-    """Test that a new post is created."""
+    """Test that edited text maintains min length conditions."""
     params = {
         'title': 'Testing',
         'text': 'T'
@@ -169,19 +169,19 @@ def test_edit_entry_min_text_len(dbtransaction, app, new_model):
 
 
 def test_edit_route(dbtransaction, app, new_model):
-    """Test home view dictionary title attribute."""
+    """Test edit route response status."""
     response = app.get('/edit/{}'.format(new_model.id))
     assert response.status_code == 200
 
 
 def test_entry_route(dbtransaction, app, new_model):
-    """Test home view dictionary title attribute."""
+    """Test entry route response status."""
     response = app.get('/entry/{}'.format(new_model.id))
     assert response.status_code == 200
 
 
 def test_bad_route(dbtransaction, app):
-    """Test home view dictionary title attribute."""
+    """Test bad route requests."""
     with pytest.raises(AppError):
         app.get('/entry/{}'.format('whatever'))
 
